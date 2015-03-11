41533f7a3143031376c20e88d289335a45bae117
ahhh <ahhh.db@gmail.com>
2015-03-10 22:29:06 -0700

    Update server.js
    Updated with socketIO that works

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-var restify = require('restify');
-var userSave = require('save')('user');
+var https = require('https');
+var sockjs = require('sockjs');
+var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var _ = require('lodash');
+var key = fs.readFileSync('./server.key');
+var cert = fs.readFileSync('./server.crt')
+var https_options = {
+    key: key,
+    cert: cert
+};
 
 var ADMIN_HOST = '127.0.0.1';
-var fs = require('fs');
-var https = require('https');
-
 var PORT = 443;
 
-var server = restify.createServer({
-  certificate: fs.readFileSync('server.crt'),
-  key: fs.readFileSync('server.key'),
-});
+clients = [];
 
-//server.use(restify.bodyParser());
+var socket = sockjs.createServer();
+
+// access all clients
+broadcast = function (message) {
+  clients.forEach(function (client) {
+    client.write(message)
+  });
+};
 
 function checkIP (req) {
   return ADMIN_HOST === req.connection.remoteAddress;
@@ -24,39 +35,35 @@ var connectionInitialized = function(req, res) {
               req.url, req.headers['user-agent']);
 };
 
-// Admin Controller
-server.get('/admin', function(req, res) {
-  connectionInitialized(req, res);
-  if (checkIP(req)) {
-    var html = fs.readFileSync('./admin/index.html', 'utf-8');
-    res.writeHead(200, {
-      'Content-Length': Buffer.byteLength(html),
-      'Content-Type': 'text/html'
-    });
-    res.write(html);
-    res.end();
-  } else {
-    res.send("Unauthorized"); 
-  }
+socket.on('connection', function (client) {
+  // Add client to clients array
+  clients.push(client);
+  console.log('New client connected (' + clients.length + ' total)');
+
+  client.on('close', function() {
+    // Remove from clients array
+    clients = _.without(clients, client);
+    console.log('Client disconnected (' + clients.length + ' total)');
+  });
+
+  setTimeout(function () {
+  	var outfile = fs.readFileSync('./admin/out.txt').toString();
+  	client.write(outfile)
+  }, 5000)
+
+  client.on('data', function (message) {
+    //message = JSON.parse(message);
+    console.log('SOCKET MESSAGE: ', message);
+  });
 });
 
+var app = express();
+server = https.createServer(https_options, app);
 
-// Cmd Update
-server.post('/cmdUpdate', function(req, res) {
-  connectionInitialized(req, res);
-  if (checkIP(req)) {
-    //console.log(req.body, req.params);
-  }else {
-   // res.write("Unauthorized"); 
-  }
-  res.writeHead(302, { 
-  'Location': '/admin',
-  'Content-Type': 'text/html' });
-  res.end('ok', 'utf-8');
-});
+//The following are all bot api commands
 
-// Bot Command
-server.get('/command', function(req, res) {
+// bots request commands from
+app.get('/command', function(req, res) {
   connectionInitialized(req, res);
   fs.readFile('./admin/command', function(err, content) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -65,7 +72,7 @@ server.get('/command', function(req, res) {
 });
 
 // Post of Bot Output
-server.post('/out', function(req, res) {
+app.post('/out', function(req, res) {
   connectionInitialized(req, res);
   var body = "";
     req.on('data', function (chunk) {
@@ -80,6 +87,26 @@ server.post('/out', function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('ok', 'utf-8');
 });
+
+// Cmd Update
+app.post('/cmdUpdate', function(req, res) {
+  connectionInitialized(req, res);
+  if (checkIP(req)) {
+    //console.log(req.body, req.params);
+  }else {
+   // res.write("Unauthorized"); 
+  }
+  res.writeHead(302, { 
+  'Location': '/admin',
+  'Content-Type': 'text/html' });
+  res.end('ok', 'utf-8');
+});
+
+// thing for websockets to connect over
+socket.installHandlers(server, { prefix: '/websocket' })
+
+// serve static content from the /admin/ dir at /admin
+app.use('/admin/', express.static(path.join(__dirname, '/admin')));
 
 server.listen(PORT, function() {
   console.log("Listening on port "+PORT+"....");
