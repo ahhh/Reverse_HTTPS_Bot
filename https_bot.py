620bd4b3f8025cd5dea862149869bb7573ded1d1
ahhh <ahhh.db@gmail.com>
2015-03-16 23:09:49 -0700

    Update https_bot.py

--- https_bot.py
+++ https_bot.py
@@ -14,6 +14,9 @@ botID = []
 #'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
 #'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
 headers = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  'Accept-Encoding': 'gzip, deflate, sdch', 
+  'Accept-Language': 'en-US,en;q=0.8', 
   'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'
 }
 
