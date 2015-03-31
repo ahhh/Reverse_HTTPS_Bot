605b2884baefdf1f42734cd7618d233508a25d5c
ahhh <ahhh.db@gmail.com>
2015-03-31 02:05:29 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# easy setup script for generating self signed ssl certificates
 openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
 openssl rsa -passin pass:x -in server.pass.key -out server.key
 rm server.pass.key
